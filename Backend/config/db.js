a48854da5fa3dfdd59a925ce36f20e4cf6cde9e9
Kedar.K <kedarkodgire.kk@gmail.com>
2021-01-17 22:41:54 +0530

    connected to db

---
+++ Backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(
+      `mongo DB connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
-import products from '../products'
-import Product from '../Components/Product'
+import React, { useState, useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+import Product from "../Components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
-    return (
-        <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map( product => (
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
-}
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/products")
+      .then((fetchProducts) => setProducts(fetchProducts.data));
+  }, []);
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
